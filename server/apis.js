6fb67b75d60bb18b535f0c1371f9fe5108904350
venukotamraju <kotamraju.venugopal@gmail.com>
2024-03-10 15:19:07 +0530

    added a port listener in the apis.js file

--- server/apis.js
+++ server/apis.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 app.get("/helloWorld", (req, res) => {
   res.json("hello world");
 });
+app.listen(port, () => console.log("listening on port ", port));
